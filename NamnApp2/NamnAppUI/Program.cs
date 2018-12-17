0c68c932c2f0953dcf7f469ffa9ea58f93739cc3
Daniel Johansson <johansson.gd@gmail.com>
2018-12-17 19:20:30 +0100

    Första

---
+++ NamnApp2/NamnAppUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamnAppUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key;
+            do
+            {
+
+            } while (key != '6');
+
+        }
+
+        public static
+    }
+}
